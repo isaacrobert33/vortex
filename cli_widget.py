585e5d001fbe83780875f62ab4095c7a7aca5ee8
Isaac Robert <isaacrobertoluwaseun@gmail.com>
2023-04-15 20:16:42 +0100

    ...

---
+++ cli_widget.py
@@ -0,0 +1,66 @@
+import sys
+from PySide2.QtWidgets import (
+    QApplication,
+    QWidget,
+    QTextEdit,
+    QVBoxLayout,
+    QPushButton,
+    QFileDialog,
+)
+from PySide2.QtCore import QProcess
+
+
+class Editor(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # Create a text edit widget to display the output
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setReadOnly(True)
+
+        # Create a text edit widget to input the commands
+        self.input_edit = QTextEdit(self)
+
+        # Create a button to open the editor
+        self.button = QPushButton("Open Editor", self)
+        self.button.clicked.connect(self.open_editor)
+
+        # Create a layout for the widgets
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.text_edit)
+        layout.addWidget(self.input_edit)
+        layout.addWidget(self.button)
+
+    def open_editor(self):
+        # Create a QProcess object to run the CLI editor
+        editor_process = QProcess(self)
+
+        # Set the program to run as the CLI editor (e.g., nano or vim)
+        editor_process.setProgram("nano")
+
+        # Start the process
+        editor_process.start()
+
+        # Redirect the output of the process to the text edit widget
+        editor_process.readyReadStandardOutput.connect(self.read_output)
+
+        # Redirect the input of the process to the input edit widget
+        editor_process.setProcessChannelMode(QProcess.MergedChannels)
+        self.input_edit.textChanged.connect(
+            lambda: editor_process.write(self.input_edit.toPlainText().encode())
+        )
+
+    def read_output(self):
+        # Read the output of the process and display it in the text edit widget
+        output = self.sender().readAllStandardOutput().data()
+        output_str = QString.fromLocal8Bit(output)
+        self.text_edit.append(output_str)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    editor = Editor()
+    editor.show()
+    sys.exit(app.exec_())
