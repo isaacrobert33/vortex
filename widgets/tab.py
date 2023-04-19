2720b19409f74497089d2a3d7f0d61c28cd8bf36
Isaac Robert <isaacrobertoluwaseun@gmail.com>
2023-04-19 17:20:04 +0100

    Tabs fixed

--- widgets/tab.py
+++ widgets/tab.py
@@ -12,12 +12,12 @@ import subprocess
 import select
 
 HOME_DIR = os.path.expanduser("~")
-SHELL_HISTORY_FILENAME = ".bash_history"
+SHELL_HISTORY_FILENAME, SHELL = ".bash_history", "bash"
 
 if os.environ["SHELL"].endswith("zsh"):
-    SHELL_HISTORY_FILENAME = ".zsh_history"
+    SHELL_HISTORY_FILENAME, SHELL = ".zsh_history", "zsh"
 elif os.environ["SHELL"].endswith("ksh"):
-    SHELL_HISTORY_FILENAME = ".sh_history"
+    SHELL_HISTORY_FILENAME, SHELL = ".sh_history", "sh"
 
 current_cmd = None
 executed = False
@@ -129,53 +129,65 @@ class ShellReader(QThread):
         executed = False
 
 
-class StdIn(QLineEdit):
+with open(os.path.join(HOME_DIR, SHELL_HISTORY_FILENAME), "r") as f:
+    shell_history = f.read().split("\n")
+
+    for i, c in enumerate(shell_history):
+        if ";" in c:
+            shell_history[i] = c.split(";")[1]
+
+
+class StdIn(QTextEdit):
+    returnPressed = Signal(str)
+
     def __init__(self, parent) -> None:
         super().__init__(parent=parent)
         self.parent = parent
         self.currentIndex = None
 
-        with open(os.path.join(HOME_DIR, SHELL_HISTORY_FILENAME), "r") as f:
-            self.shell_history = f.read().split("\n")[::-1]
+        self.setFocus()
 
     def keyPressEvent(self, event) -> None:
         if event.key() == Qt.Key_Up:
             self.navigate_up()
         elif event.key() == Qt.Key_Down:
             self.navigate_down()
-
-        super().keyPressEvent(event)
+        elif event.key() == Qt.Key_Return or event.key() == Qt.Key_Enter:
+            self.returnPressed.emit(self.toPlainText())
+        else:
+            super().keyPressEvent(event)
 
     def navigate_up(self):
-        t = self.text().strip()
-        if not t:
-            self.shell_history.append(t)
-            self.setText(self.shell_history[-2].strip())
-            self.currentIndex = -2
+        t = self.toPlainText().strip()
+        if not self.currentIndex:
+            shell_history.append(t)
+            self.setText(shell_history[-2].strip())
+            self.currentIndex = len(shell_history) - 2
         else:
             if not self.currentIndex == 0:
                 self.currentIndex -= 1
-                self.setText(self.shell_history[self.currentIndex])
+                self.setText(shell_history[self.currentIndex])
 
-        self.setCursorPosition(len(self.text()))
+        self.textCursor().movePosition(self.textCursor().End)
 
     def navigate_down(self):
-        t = self.text().strip()
-        if t and self.currentIndex is not None:
-            if not self.currentIndex == len(self.shell_history):
-                self.currentIndex += 1
-                self.setText(self.shell_history[self.currentIndex])
+        t = self.toPlainText().strip()
 
-        self.setCursorPosition(len(self.text()))
+        if t and self.currentIndex is not None:
+            if not self.currentIndex == len(shell_history):
+                self.currentIndex += 1
+                self.setText(shell_history[self.currentIndex])
+
+        self.textCursor().movePosition(self.textCursor().End)
 
 
 class UiTab(QWidget):
     def __init__(
-        self,
-        parent: typing.Optional[QWidget] = ...,
-        mainwindow: typing.Optional[QMainWindow] = ...,
+        self, parent, mainwindow: typing.Optional[QMainWindow] = ..., tab_index=None
     ) -> None:
         super().__init__(parent)
+        self.parent = parent
+        self.tab_index = tab_index
 
         self.setObjectName("tab")
         self.mainwindow = mainwindow
@@ -189,14 +201,11 @@ class UiTab(QWidget):
         self.stdout.setObjectName("stdout")
         self.stdout.setGeometry(QRect(-5, 0, 960, 540))
         self.stdout.setStyleSheet(
-            "border: 1px solid gray; background-color: transparent;"
+            'border: 1px solid gray; background-color: transparent;font: 13pt "Courier New";'
         )
         self.stdout.setReadOnly(True)
         self.stdout.setWindowOpacity(0.5)
-
-        self.runtime_stdout = QTextEdit(self)
-        self.runtime_stdout.setGeometry(QRect(-5, 560, 960, 0))
-        self.setVisible(False)
+        self.stdout.setAlignment(Qt.AlignBottom)
 
         # Create a directory label
         self.current_dir_label = QLabel(self)
@@ -213,7 +222,7 @@ class UiTab(QWidget):
         self.stdin.setGeometry(QRect(0, 575, 955, 38))
         self.stdin.setStyleSheet(
             """border: none;
-            QLineEdit::focus {border: none; }"""
+            font: 75 bold 13pt "Courier New";"""
         )
         self.stdin.setWindowOpacity(0.5)
         # Connect the returnPressed signal of the input widget to the executeCommand slot
@@ -223,37 +232,45 @@ class UiTab(QWidget):
         self.shell_proc.started.connect(self.thread_setup)
         self.shell_proc.start()
 
+    def keyPressEvent(self, event) -> None:
+        if event.key() == Qt.Key_C and event.modifiers() == Qt.ControlModifier:
+            print("Ctrl+C was pressed")
+
+        return super().keyPressEvent(event)
+
     def thread_setup(self, dt=None):
         self.shell_proc.reader.cmd_stdout.connect(self.updateOutput)
         self.shell_proc.reader.exec_done.connect(self.executed)
 
-    def executeCommand(self):
+    def executeCommand(self, ls=None):
         global current_cmd
         # Get the command to be executed from the input widget
-        self.command = self.stdin.text().strip()
+        self.command = self.stdin.toPlainText().strip()
+        shell_history.append(self.command)
 
-        # stdout = f"{self.stdout.toHtml().replace('</html>', '')}<hr>{self.currentDir}<br><b>{self.command}</b><br></html>"
-        # self.runtime_stdout.setText(stdout)
+        with open(os.path.join(HOME_DIR, SHELL_HISTORY_FILENAME), "a") as f:
+            f.write(f"\n{self.command}")
+        f.close()
+
+        stdout = f"{self.stdout.toHtml().replace('</html>', '')}<hr>{self.currentDir}<br><b>{self.command}</b><br></html>"
+        self.stdout.setText(stdout)
 
         if "exit" in self.command:
-            self.mainwindow.close()
+            self.parent.closeTab(self.tab_index)
 
         current_cmd = self.command
         self.stdin.setDisabled(True)
-        self.runtime_stdout.setVisible(True)
 
     def updateOutput(self, output):
         # Append the output to the text edit widget
-        cursor = self.runtime_stdout.textCursor()
+        cursor = self.stdout.textCursor()
         cursor.movePosition(cursor.End)
         cursor.insertText(output + "\n")
-        self.runtime_stdout.setTextCursor(cursor)
-        self.adjust_height()
+        self.stdout.setTextCursor(cursor)
 
     def executed(self, state):
         if "cd " in self.command and state:
             self.currentDir = os.getcwd().replace(HOME_DIR, "~")
-            print(self.currentDir)
             self.current_dir_label.setText(self.currentDir)
         elif "clear" in self.command:
             self.stdout.setText("")
@@ -263,12 +280,8 @@ class UiTab(QWidget):
         self.stdin.setDisabled(False)
         self.stdin.setFocus()
         self.stdout.setText(
-            f"{self.runtime_stdout.toHtml().replace('</html>', '')}<hr>{self.currentDir}<br><b>{self.command}</b><br></html>"
+            f"{self.stdout.toHtml().replace('</html>', '')}<hr>{self.currentDir}<br><b>{self.command}</b><br></html>"
         )
-        self.runtime_stdout.setVisible(False)
-        self.runtime_stdout.setText("")
-        self.current_dir_label.move(QPoint(0, 555))
-        self.stdin.move(QPoint(0, 575))
 
     def adjust_height(self):
         content_height = self.runtime_stdout.document().size().height()
--- widgets/tabs.py
+++ widgets/tabs.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 from PySide2.QtWidgets import (
     QTabWidget,
     QWidget,
@@ -6,6 +6,7 @@ from PySide2.QtWidgets import (
 )
 from PySide2.QtGui import QIcon
 from PySide2.QtCore import QRect, Qt
+from widgets.tab import UiTab
 
 
 class UiTabs(QTabWidget):
@@ -13,14 +14,28 @@ class UiTabs(QTabWidget):
         super().__init__(movable=True, tabsClosable=True, usesScrollButtons=True)
         self.mainwindow = mainwindow
         self.new_tab_btn = QPushButton("+")
+        self.new_tab_btn.setStyleSheet('font: 75 bold 20pt "Courier New";')
+        self.new_tab_btn.clicked.connect(self.create_new_tab)
 
         self.setTabShape(self.Rounded)
         self.setCornerWidget(self.new_tab_btn, corner=Qt.TopRightCorner)
         self.setStyleSheet("background-color: transparent;")
 
-    def create_new_tab(self, tab_name, tab_widget: QWidget):
+        self.tabCloseRequested.connect(self.closeTab)
+
+    def closeTab(self, index):
+        # Get the widget at the specified index
+        widget = self.widget(index)
+        # Remove the tab and its associated widget
+        self.removeTab(index)
+        widget.deleteLater()
+
+        if self.count() == 0:
+            self.mainwindow.close()
+
+    def create_new_tab(self, tab_name=os.getcwd(), tab_widget: QWidget = UiTab):
         # Add new tab to the tab widget
-        tab = tab_widget(self, self.mainwindow)
+        tab = tab_widget(self, self.mainwindow, tab_index=self.currentIndex() + 1)
         self.addTab(tab, tab_name)
 
         return self.currentIndex() + 1
